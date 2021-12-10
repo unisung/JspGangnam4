a050aaab5f1609d99e2c0718070404595486e595
user <user@DESKTOP-S6ON2JF>
2021-12-10 16:01:20 +0900

    a

---
+++ ch18_mvc/src/ch18_mvc/MyController.java
@@ -0,0 +1,29 @@
+package ch18_mvc;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MyController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+			  throws ServletException, IOException {
+        //view로 전달하기 위한 객체 설정
+		request.setAttribute("message", "Hello! Java Server Page.");
+		RequestDispatcher rd = request.getRequestDispatcher("view.jsp");
+		rd.forward(request, response);
+	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}

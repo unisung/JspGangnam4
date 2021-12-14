ace0c86879271b864202c3dd11d98e6384fc0b85
user <user@DESKTOP-S6ON2JF>
2021-12-14 12:28:25 +0900

    a

--- WebMarket/src/mvc/controller/BoardController.java
+++ WebMarket/src/mvc/controller/BoardController.java
@@ -130,7 +130,6 @@ public class BoardController extends HttpServlet {
 		request.setAttribute("total_page", total_page);
 		request.setAttribute("total_record", total_record);
 		request.setAttribute("boardlist", boardList);
-	
 	}
 
 	//인증된 사용자명 얻기

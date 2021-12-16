0af622e66260b3c692e36af27c4f5fff6296f207
user <user@DESKTOP-S6ON2JF>
2021-12-16 11:39:09 +0900

    a

--- WebMarket/src/mvc/controller/BoardController.java
+++ WebMarket/src/mvc/controller/BoardController.java
@@ -1,10 +1,16 @@
 package mvc.controller;
 
+import java.io.FileReader;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Set;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -13,6 +19,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import mvc.command.Command;
 import mvc.model.BoardDAO;
 import mvc.model.BoardDTO;
 
@@ -21,6 +28,39 @@ public class BoardController extends HttpServlet {
 
 	//게시글 페이지당 조회결과 건수 상수 선언
 	static final int LISTCOUNT = 10;
+	
+	//클래스들을 Key는 command이고 객체는 Command타입으로 저장하기위한 map생성 
+	private Map<String,Command> commandMap = new HashMap<>();
+
+	//tomcat 기동시 command객체들을 만들어서 commandMap에 저장
+	@Override
+	public void init() throws ServletException {
+	 //web.xml의 init-param값 읽어 처리
+		String configFile = getInitParameter("configFile");
+		System.out.println("configFile:"+configFile);
+		//<문자열,문자열> 값 읽기 <- properties
+		Properties prop = new Properties();
+		//File system상의 물리적인 경로
+		String configFilePath = getServletContext().getRealPath(configFile);
+		System.out.println("configFilePath:"+configFilePath);
+		//File system의 properties파일과 문자단위의 입력 스트림 경로 설정(객체 생성)
+	    try(FileReader fis =new FileReader(configFilePath)) {
+	    	   //properties객체로 저장하기
+	    	prop.load(fis);
+	    /*  System.out.println(prop.get("/BoardListAction.do"));	
+	      Set keySet = prop.keySet();
+	      Iterator itor = keySet.iterator();
+	      while(itor.hasNext()) {
+	    	  String key = (String)itor.next();
+	    	  System.out.println(key+"="+prop.get(key));
+	      } */
+	      
+	      
+	    }catch(Exception e) {
+	    	
+	    }
+	}
+	
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
 			throws ServletException, IOException {
@@ -288,4 +328,5 @@ public class BoardController extends HttpServlet {
 		//DAO에서 DB에 저장하기 위해 메소드 호출
 		dao.insertBoard(board);
 	}
+
 }

2b85248aad06a6681f6b5cf5247b41ea6cfe4514
user <user@DESKTOP-S6ON2JF>
2021-12-21 15:41:14 +0900

    a

--- WebMarket/src/mvc/bbs/model/BbsDAO.java
+++ WebMarket/src/mvc/bbs/model/BbsDAO.java
@@ -385,5 +385,76 @@ public int getLastNum() {
 		  }
 	  } 
 	return maxNum;
-  }//getLastNum() 끝.
+ }//getLastNum() 끝.
+
+//Bbs 수정 처리
+public void updateBbs(BbsDTO bbs) {
+	Connection conn = null;
+	PreparedStatement pstmt = null;
+	
+	String sql = "update bbs set writer=?,subject=?,content=?,ip=? where num=?";			
+	
+	
+	try { //신규글 등록 처리
+		conn = DBConnectionOracle.getConnection();
+		
+			//댓글 입력 처리
+			pstmt = conn.prepareStatement(sql);
+			System.out.println("sql:"+sql);
+			
+			pstmt.setString(1, bbs.getWriter());
+			pstmt.setString(2, bbs.getSubject());
+			pstmt.setString(3, bbs.getContent());
+			pstmt.setString(4, bbs.getIp());
+            pstmt.setInt(5, bbs.getNum());			
+			//update처리
+			pstmt.executeUpdate();
+			
+ }catch(Exception e){
+	  System.out.println("에러:"+e);
+  }finally {
+	  try {
+		    if(pstmt!=null) pstmt.close();
+		    if(conn!=null)conn.close();
+	  }catch(Exception e) {
+		  throw new RuntimeException(e.getMessage());
+	  }
+  } 	
+ }//updateBbs() 끝.
+
+//Bbs 삭제 처리
+public void deleteBbs(int num) {
+	Connection conn = null;
+	PreparedStatement pstmt = null;
+	ResultSet rs=null;
+	
+	String replyCountSql="select count(*) from bbs where ref=? and re_step >0";
+	String sql = "delete from  bbs where num=?";			
+	
+	try { //삭제 처리
+		conn = DBConnectionOracle.getConnection();
+		
+		System.out.println("sql:"+sql);
+			//댓글 존재 여부확인
+			pstmt = conn.prepareStatement(replyCountSql);
+			pstmt.setInt(1, num);
+			rs = pstmt.executeQuery();
+            int replyCount=0;
+			if(rs.next()) replyCount = rs.getInt(1);
+			if(replyCount ==0 ) { //댓글이 없는 글들만 삭제
+				pstmt = conn.prepareStatement(sql);
+				pstmt.setInt(1, num);			
+                pstmt.executeUpdate();
+			}
+}catch(Exception e){
+	  System.out.println("에러:"+e);
+}finally {
+	  try {
+		    if(pstmt!=null) pstmt.close();
+		    if(conn!=null)conn.close();
+	  }catch(Exception e) {
+		  throw new RuntimeException(e.getMessage());
+	  }
+ } 	
+}//updateBbs() 끝.
 }
